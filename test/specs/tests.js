05a5eb755cffe21de6ea32c3581d27af4471a473
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2014-10-03 19:12:16 -0400

    finished file structure and package.json

---
+++ test/specs/tests.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('Udacity API Test Suite', function() {
+    it('does not have any tests yet', function() {
+        expect(1).to.equal(1);
+    });
+});
